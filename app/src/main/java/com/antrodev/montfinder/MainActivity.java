9d441c840ef0ffd81bacc471900f5622503c0db8
antoinedanois <antoine.danoislycee@gmail.com>
2016-01-11 14:52:16 +0100

    Merge branch 'master' of /Users/antoinedanois/AndroidStudioProjects/MontFinder with conflicts.

--- app/src/main/java/com/antrodev/montfinder/MainActivity.java
+++ app/src/main/java/com/antrodev/montfinder/MainActivity.java
@@ -2,24 +2,15 @@ package com.antrodev.montfinder;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
-import android.widget.TextView;
-
-import com.antrodev.montfinder.db.Sommet;
-import com.antrodev.montfinder.db.SommetDatabaseHandler;
-
-import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
-    SommetDatabaseHandler dbMan;
-    List<Sommet> sommets;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        dbMan=SommetDatabaseHandler.getSommetDatabaseHandler(this);
-        dbMan.initializeValues();
-        sommets=dbMan.getSommets();
+
+
+
     }
 }
