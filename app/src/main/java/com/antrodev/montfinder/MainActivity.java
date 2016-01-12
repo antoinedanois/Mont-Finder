ed21e02531b7bb7430a242c5119746817382ad89
antoinedanois <antoine.danoislycee@gmail.com>
2016-01-12 09:57:41 +0100

    Commit Boussole/GPS

--- app/src/main/java/com/antrodev/montfinder/MainActivity.java
+++ app/src/main/java/com/antrodev/montfinder/MainActivity.java
@@ -6,6 +6,7 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.graphics.Point;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
@@ -13,8 +14,14 @@ import android.os.Build;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.DisplayMetrics;
+import android.view.Display;
 import android.view.View;
+import android.widget.AbsoluteLayout;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.ProgressBar;
+import android.widget.RadioGroup;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -35,8 +42,11 @@ public class MainActivity extends Activity {
     float[] orientation;
     int xAxisDegrees;
 
+
+    ImageView ivArrow = null;
     TextView tvLatLong = null;
     ProgressBar progressBar = null;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -75,10 +85,13 @@ public class MainActivity extends Activity {
             public void onReceive(Context context, Intent intent) {
                 orientation = new float[]{intent.getFloatExtra(OrientationPrecise.CLEF_ORIENTATION_0, 0), intent.getFloatExtra(OrientationPrecise.CLEF_ORIENTATION_1,0), intent.getFloatExtra(OrientationPrecise.CLEF_ORIENTATION_2, 0)};
                 float xAxis = (float) (orientation[0]*180/Math.PI+90);
-                System.out.println("Boussole = " + xAxisDegrees);
+
                 tvLatLong.setText("Boussole = " + xAxisDegrees);
 
                 xAxisDegrees  = Math.round(xAxis);
+
+                updateNord();
+
             }
         };
 
@@ -91,6 +104,23 @@ public class MainActivity extends Activity {
                 showLocation(intent);
             }
         };
+    }
+
+    private void updateNord(){
+
+
+        Display display = getWindowManager().getDefaultDisplay();
+        Point size = new Point();
+        display.getSize(size);
+        int tailleEcran = size.x;
+
+        int test = ivArrow.getWidth()/2;
+
+
+        if(xAxisDegrees>-30 && xAxisDegrees<30){
+            ivArrow.setX((xAxisDegrees*(-tailleEcran/60))+((tailleEcran/2)-test));
+        }
+
     }
 
 
@@ -112,6 +142,8 @@ public class MainActivity extends Activity {
         registerReceiver(br, new IntentFilter(LocalisationGPS.ACTION_LOCATION));
         tvLatLong = (TextView) findViewById(R.id.textViewLatLong);
         progressBar = (ProgressBar) findViewById(R.id.progressBar);
+        ivArrow = (ImageView) findViewById(R.id.imageViewArrow);
+
 
         tvLatLong.setText("Localisation par GPS en cours...");
 
