312465a6962ca66aef8c7e59dc75b98ee6078e72
antoinedanois <antoine.danoislycee@gmail.com>
2016-01-12 09:00:36 +0100

    Commit Boussole/GPS

---
+++ app/src/main/java/com/antrodev/montfinder/LocalisationGPS.java
@@ -0,0 +1,92 @@
+package com.antrodev.montfinder;
+
+import android.Manifest;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.util.Log;
+import android.widget.Toast;
+
+/**
+ * Created by antoinedanois on 11/01/2016.
+ */
+public class LocalisationGPS {
+    static LocalisationGPS localisationGPS = null;
+    static final String ACTION_LOCATION = "loc";
+    Context appContext;
+    LocationManager locationManager;
+
+    private LocalisationGPS(Context appContext) {
+        this.appContext = appContext;
+        locationManager = (LocationManager) appContext.getSystemService(Context.LOCATION_SERVICE);
+    }
+
+    public static LocalisationGPS get(Context context) {
+        if (localisationGPS == null) {
+            localisationGPS = new LocalisationGPS(context);
+        }
+        return localisationGPS;
+    }
+
+    public void startLocationUpdates() {
+        try {
+            LocationListener locl = new LocationListener() {
+                @Override
+                public void onLocationChanged(Location location) {
+                    if (location != null) {
+                        System.out.println("Passage là aussi" + location.getLatitude());
+
+                        Log.d(this.getClass().getName(), location.toString());
+                        broadcastLocation(location);
+
+                    }
+                }
+
+                @Override
+                public void onStatusChanged(String provider, int status, Bundle extras) {
+
+                }
+
+                @Override
+                public void onProviderEnabled(String provider) {
+
+                }
+
+                @Override
+                public void onProviderDisabled(String provider) {
+
+                }
+            };
+
+            System.out.println("Passage là");
+
+
+            if (ActivityCompat.checkSelfPermission(appContext, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(appContext, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                // TODO: Consider calling
+                System.out.println("Passage ici");
+
+                return;
+            }else{
+                System.out.println("Coucou");
+                locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 1000, 1, locl);
+            }
+
+
+            } catch (Exception e){
+                Log.e("LocalisationGPS","Erreur securite" + e);
+            }
+        }
+
+        private void broadcastLocation(Location location) {
+            Intent broadcast = new Intent(ACTION_LOCATION);
+            broadcast.putExtra(LocationManager.KEY_LOCATION_CHANGED, location);
+            appContext.sendBroadcast(broadcast);
+        }
+    }
--- app/src/main/java/com/antrodev/montfinder/MainActivity.java
+++ app/src/main/java/com/antrodev/montfinder/MainActivity.java
@@ -1,12 +1,22 @@
 package com.antrodev.montfinder;
 
+import android.Manifest;
 import android.app.Activity;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Build;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.antrodev.montfinder.db.Sommet;
 import com.antrodev.montfinder.db.SommetDatabaseHandler;
@@ -18,19 +28,30 @@ public class MainActivity extends Activity {
 
     SommetDatabaseHandler dbMan;
     List<Sommet> sommets;
+    LocalisationGPS lGPS = null;
+    BroadcastReceiver br;
     OrientationPrecise op;
     BroadcastReceiver orientationReceiver;
     float[] orientation;
-    float xAxisDegrees;
+    int xAxisDegrees;
+
+    TextView tvLatLong = null;
+    ProgressBar progressBar = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_camera);
 
+        tvLatLong = (TextView) findViewById(R.id.textViewLatLong);
+
+        lGPS = LocalisationGPS.get(this.getApplicationContext());
+        lGPS.startLocationUpdates();
+
 
         dbMan=SommetDatabaseHandler.getSommetDatabaseHandler(this);
-        dbMan.initializeValues();
+
+        //dbMan.initializeValues();
         sommets=dbMan.getSommets();
 
 
@@ -46,19 +67,54 @@ public class MainActivity extends Activity {
                     .commit();
         }
 
+
         op=OrientationPrecise.getOrientationPrecise(this);
-        orientationReceiver=new BroadcastReceiver() {
+
+        orientationReceiver = new BroadcastReceiver() {
             @Override
             public void onReceive(Context context, Intent intent) {
-                orientation=new float[]{intent.getFloatExtra(OrientationPrecise.CLEF_ORIENTATION_0,0),intent.getFloatExtra(OrientationPrecise.CLEF_ORIENTATION_1,0),intent.getFloatExtra(OrientationPrecise.CLEF_ORIENTATION_2,0)};
-                xAxisDegrees= (float) (orientation[0]*180/Math.PI+90);
+                orientation = new float[]{intent.getFloatExtra(OrientationPrecise.CLEF_ORIENTATION_0, 0), intent.getFloatExtra(OrientationPrecise.CLEF_ORIENTATION_1,0), intent.getFloatExtra(OrientationPrecise.CLEF_ORIENTATION_2, 0)};
+                float xAxis = (float) (orientation[0]*180/Math.PI+90);
+                System.out.println("Boussole = " + xAxisDegrees);
+                tvLatLong.append("\nBoussole = " + xAxisDegrees);
+
+                xAxisDegrees  = Math.round(xAxis);
             }
         };
+
+
+
+
+        br=new BroadcastReceiver() {
+            @Override
+            public void onReceive(Context context, Intent intent) {
+                showLocation(intent);
+            }
+        };
+    }
+
+
+    private void showLocation(Intent intent){
+        Location loc =(Location) intent.getParcelableExtra(LocationManager.KEY_LOCATION_CHANGED);
+        if(loc!=null){
+            //Toast.makeText(getApplicationContext(), "Latitude = " + loc.getLatitude() + " Longitude = " + loc.getLongitude(), Toast.LENGTH_SHORT).show();
+
+            progressBar.setVisibility(View.INVISIBLE);
+            tvLatLong.setText("Latitude = " + loc.getLatitude() + " Longitude = " + loc.getLongitude());
+
+        }
     }
 
     @Override
     protected void onResume() {
         super.onResume();
+        LocalisationGPS.get(this).startLocationUpdates();
+        registerReceiver(br, new IntentFilter(LocalisationGPS.ACTION_LOCATION));
+        tvLatLong = (TextView) findViewById(R.id.textViewLatLong);
+        progressBar = (ProgressBar) findViewById(R.id.progressBar);
+
+        tvLatLong.setText("Localisation par GPS en cours...");
+
         registerReceiver(orientationReceiver, new IntentFilter(OrientationPrecise.MESSAGE_ORIENTATION));
         op.start();
     }
@@ -66,9 +122,16 @@ public class MainActivity extends Activity {
     @Override
     protected void onPause() {
         super.onPause();
+        unregisterReceiver(br);
+
         unregisterReceiver(orientationReceiver);
         op.stop();
     }
+
+
+
+
+
 
 
 }
